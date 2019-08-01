2e92ed6dde9f0ac0ae367202fd2ed7ba1229e234
Carlosetec <carloseduardoetecab@gmail.com>
2019-08-02 03:14:02 +0900

    Botões javaScript

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,15 +1,84 @@
-// This is a JavaScript file
-$(document).on("click","#btnsomar",function(){
-var valor1 = $("#valor1").val();
-var valor2 = $("#valor2").val();
-var result = parseFloat(valor1) + parseFloat(valor2);
-$("#resultado").val(result);
+
+$(document).on("click","#valor1",function(){
+var numero1 = 1;
+ var re = $("#resultado").val();
+ $("#resultado").val(re + numero1);
 })
-$(document).on("click","#multiplicar",function(){
-var valor1 = $("#valor1").val();
-var valor2 = $("#valor2").val();
-var result = parseFloat(valor1) * parseFloat(valor2);
-$("#resultado").val(result);
+
+$(document).on("click","#valor2",function(){
+var numero2 = 2;
+ var re = $("#resultado").val();
+
+ $("#resultado").val(re + numero2);
 })
+$(document).on("click","#valor3",function(){
+var numero3 = 3;
+ var re = $("#resultado").val();
+
+ $("#resultado").val(re + numero3);
+})
+
+$(document).on("click","#valor4",function(){
+var numero4 = 4;
+ var re = $("#resultado").val();
+
+ $("#resultado").val(re + numero4);
+})
+
+$(document).on("click","#valor5",function(){
+var numero5 = 5;
+ var re = $("#resultado").val();
+
+ $("#resultado").val(re + numero5);
+})
+
+$(document).on("click","#valor6",function(){
+var numero6 = 6;
+ var re = $("#resultado").val();
+
+ $("#resultado").val(re + numero6);
+})
+
+$(document).on("click","#valor7",function(){
+var numero7 = 7;
+ var re = $("#resultado").val();
+
+ $("#resultado").val(re + numero7);
+})
+
+$(document).on("click","#valor8",function(){
+var numero8 = 8;
+ var re = $("#resultado").val();
+
+ $("#resultado").val(re + numero8);
+})
+
+$(document).on("click","#valor9",function(){
+var numero9 = 9;
+ var re = $("#resultado").val();
+
+ $("#resultado").val(re + numero9);
+})
+
+$(document).on("click","#soma",function(){
+v1= parseFloat($(resultado).val());
+$("#resultado").val("");
+var re = $("#resultado").val();
+})
+
+$(document).on("click","#apagar",function(){
+v1= parseFloat($(resultado).val());
+$("#resultado").val("");
+var re = $("#resultado").val();
+})
+$(document).on("click","#igual",function(){
+v1= parseFloat($(resultado).val());
+$("#resultado").val();
+var re = $("#resultado").val();
+})
+
+
+
+
 
 
